be82ad97f671ab3e5803ebb4fa922863d772c906
王兴欣 <evil_outman@163.com>
2020-04-15 15:48:37 +0800

    🚀 修改测试

--- tests/index.js
+++ tests/index.js
@@ -4,7 +4,7 @@
  * @Autor: 王兴欣
  * @Date: 2020-04-15 13:07:30
  * @LastEditors: 王兴欣
- * @LastEditTime: 2020-04-15 14:19:44
+ * @LastEditTime: 2020-04-15 15:07:49
  */
 
 const logger = require('../index')
@@ -17,7 +17,9 @@ logger.warn('FBI WARNING')
 
 logger.error('this is a error name', 'this is error message')
 
-logger.error('this is error message')
+// logger.error('this is error message')
+
+logger.error('error.name', {a: 'something wrong'})
 
 logger.define({
   key: 'define'
